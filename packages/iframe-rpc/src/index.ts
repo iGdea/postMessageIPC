f7026ab0be2104f9d977fd0d9478cd2c400095e6
bacrawu <bacrawu@tencent.com>
2024-07-25 14:10:15 +0800

    Add: export TransformHandler

--- packages/iframe-rpc/src/index.ts
+++ packages/iframe-rpc/src/index.ts
@@ -1,2 +1,3 @@
 export { IframeIPC, type IPCOptions } from './lib/IframeIPC';
 export { IframeMessagePromise, type MessagePromiseOptions } from './lib/IframeMessagePromise';
+export type { TransformHandler } from './lib/Message';
--- packages/iframe-rpcs/src/index.ts
+++ packages/iframe-rpcs/src/index.ts
@@ -1,30 +1,36 @@
-import { IframeIPC, type IPCOptions } from 'iframe-ipc';
 import AES from 'crypto-js/aes';
 import encUTF8 from 'crypto-js/enc-utf8';
 import Hex from 'crypto-js/enc-hex';
 import { random } from 'crypto-js/lib-typedarrays';
 
+import {
+  IframeIPC,
+  type IPCOptions,
+  type TransformHandler,
+} from 'iframe-ipc';
+
 
 export class IframeIPCs extends IframeIPC {
   constructor(namespace: string, options: IPCOptions & { aes: string | (() => string) }) {
+    const transform: TransformHandler<any, { result: string, iv: string }> = async (type, args) => {
+      const aes: string = typeof options.aes === 'function' ? options.aes() : options.aes;
+
+      if (type === 'encode') {
+        const json = options.transform ? await options.transform('encode', args) : args;
+        const iv = Hex.parse(random(16).toString());
+        const result = AES.encrypt(JSON.stringify(json), aes, { iv });
+        // console.log('encrypt', result);
+        return { result: result.toString(), iv: iv.toString() };
+      }
+
+      const json = JSON.parse(AES.decrypt(args.result, aes, { iv: args.iv }).toString(encUTF8));
+      // console.log('decrypt', json);
+      return options.transform ? options.transform('decode', json) : json;
+    };
+
     super(namespace, {
       ...options,
-
-      async transform(type, args) {
-        const aes: string = typeof options.aes === 'function' ? options.aes() : options.aes;
-
-        if (type === 'encode') {
-          const json = options.transform ? await options.transform('encode', args) : args;
-          const iv = Hex.parse(random(16).toString());
-          const result = AES.encrypt(JSON.stringify(json), aes, { iv });
-          // console.log('encrypt', result);
-          return { result: result.toString(), iv: iv.toString() };
-        }
-
-        const json = JSON.parse(AES.decrypt(args.result, aes, { iv: args.iv }).toString(encUTF8));
-        // console.log('decrypt', json);
-        return options.transform ? options.transform('decode', json) : json;
-      }
+      transform,
     });
   }
 }
