fa7c0ceec0b29622e98981568b873c40d581e23f
bacrawu <bacrawu@tencent.com>
2024-07-25 12:28:23 +0800

    Add: 增加 options.aes func

--- packages/iframe-rpcs/src/index.ts
+++ packages/iframe-rpcs/src/index.ts
@@ -2,20 +2,23 @@ import { IframeIPC, type IPCOptions } from 'iframe-ipc';
 import AES from 'crypto-js/aes';
 import encUTF8 from 'crypto-js/enc-utf8';
 
+
 export class IframeIPCs extends IframeIPC {
-  constructor(namespace: string, options: IPCOptions & { aes: string }) {
+  constructor(namespace: string, options: IPCOptions & { aes: string | (() => string) }) {
     super(namespace, {
       ...options,
 
       async transform(type, args) {
+        const aes: string = typeof options.aes === 'function' ? options.aes() : options.aes;
+
         if (type === 'encode') {
           const json = options.transform ? await options.transform('encode', args) : args;
-          const result = AES.encrypt(JSON.stringify(json), options.aes).toString();
+          const result = AES.encrypt(JSON.stringify(json), aes).toString();
           // console.log('encrypt', result);
           return result;
         }
 
-        const json = JSON.parse(AES.decrypt(args, options.aes).toString(encUTF8));
+        const json = JSON.parse(AES.decrypt(args, aes).toString(encUTF8));
         // console.log('decrypt', json);
         return options.transform ? options.transform('decode', json) : json;
       }
